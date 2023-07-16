cb34030ba154d8ed699c23a7ee254f4d1a8ea2ab
Seikirin <81483357+Seikirin@users.noreply.github.com>
2023-07-16 11:50:32 +0000

    a

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: mcharrad <mcharrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 10:27:06 by mcharrad          #+#    #+#             */
-/*   Updated: 2023/07/16 11:25:36 by mcharrad         ###   ########.fr       */
+/*   Updated: 2023/07/16 11:50:21 by mcharrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,42 @@ Intern &Intern::operator=(const Intern &copy)
 	return *this;
 }
 
+static AForm *makeRobotomyRequestForm(std::string target)
+{
+	return new RobotomyRequestForm(target);
+}
+
+static AForm *makePresidentialPardonForm(std::string target)
+{
+	return new PresidentialPardonForm(target);
+}
+
+static AForm *makeShrubberyCreationForm(std::string target)
+{
+	return new ShrubberyCreationForm(target);
+}
+
 AForm *Intern::makeForm(std::string form, std::string target)
 {
-	AForm *f;
-	form == "robotomy request"			? f = new RobotomyRequestForm(target):
-	form == "presidential pardon"		? f = new PresidentialPardonForm(target):
-	form == "shrubbery creation"		? f = new ShrubberyCreationForm(target):
-	f = 0;
-	std::cout << (f ? "Intern creates " : "Intern could not create ") << form << std::endl;
-	return f;
+	struct s_form
+	{
+		std::string name;
+		AForm *(*f)(std::string);
+	} forms[] = {
+		{"robotomy request", makeRobotomyRequestForm},
+		{"presidential pardon", makePresidentialPardonForm},
+		{"shrubbery creation", makeShrubberyCreationForm},
+	};
+
+	for (size_t i = 0; i < sizeof(forms) / sizeof(forms[0]) ; i++)
+	{
+		if (forms[i].name == form)
+		{
+			std::cout << "Intern creates " << form << std::endl;
+			return forms[i].f(target);
+		}
+	}
+
+	std::cout << "Intern could not create " << form << std::endl;
+	return 0;
 }
